94c0aa63953ec6c874e9a8559e11cc67df5f8ff2
Iain Mckay <iainmckay@ecenglish.com>
2015-01-09 10:48:05 +0100

    Fixed bad comparison

--- start.sh
+++ start.sh
@@ -13,6 +13,8 @@ fi
 
 etcd-dump -h $ETCD_IP -p $ETCD_PORT -f /tmp/dump.json $MODE
 
-if [ $? = 0 ]; then
+if [ $? == 0 ]; then
 	aws s3 cp /tmp/dump.json s3://$S3_BUCKET/$S3_OBJECT
+else
+	exit $?
 fi
