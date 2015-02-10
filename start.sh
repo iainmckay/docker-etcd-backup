759b4966a34656df495c6671d47d04ee2f6950ae
Iain Mckay <iainmckay@ecenglish.com>
2015-02-10 10:14:56 +0100

    Indentation

--- start.sh
+++ start.sh
@@ -3,18 +3,18 @@
 MODE=
 
 if [ "$1" == "backup" ]; then
-	MODE='dump'
+    MODE='dump'
 elif [ "$1" == "restore" ]; then
-	MODE='restore'
+    MODE='restore'
 else
-	>&2 echo "You must provide either backup or restore to run this container"
-	exit 64
+    >&2 echo "You must provide either backup or restore to run this container"
+    exit 64
 fi
 
 etcd-dump -h $ETCD_IP -p $ETCD_PORT -f /tmp/dump.json $MODE
 
 if [ $? == 0 ]; then
-	aws s3 cp /tmp/dump.json s3://$S3_BUCKET/$S3_OBJECT
+    aws s3 cp /tmp/dump.json s3://$S3_BUCKET/$S3_OBJECT
 else
-	exit $?
+    exit $?
 fi
