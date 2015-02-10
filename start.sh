1f2dda43dc34ba1d8bd387237e6ac77764afa98a
Iain Mckay <iainmckay@ecenglish.com>
2015-02-10 10:18:21 +0100

    Restore command didn't actually restore

--- start.sh
+++ start.sh
@@ -11,10 +11,27 @@ else
     exit 64
 fi
 
+if ["$MODE" == "restore"]; then
+    aws s3 cp s3://$S3_BUCKET/$S3_OBJECT /tmp/dump.json
+
+    if [ $? != 0]; then
+        >&2 echo "There was a problem fetching the backup from S3"
+        exit $?
+    fi
+fi
+
 etcd-dump -h $ETCD_IP -p $ETCD_PORT -f /tmp/dump.json $MODE
 
-if [ $? == 0 ]; then
-    aws s3 cp /tmp/dump.json s3://$S3_BUCKET/$S3_OBJECT
-else
+if [ $? != 0]; then
     exit $?
 fi
+
+if ["$MODE" == "backup"]; then
+    aws s3 cp /tmp/dump.json s3://$S3_BUCKET/$S3_OBJECT
+
+    if [ $? != 0]; then
+        exit $?
+    fi
+fi
+
+exit 0
